5699168e63dab8533f401641261cc8602665e8d5
Juan Kineipe <juan.kineipe@gmail.com>
2020-05-04 12:09:14 -0300

    Revert "Adicionado botão de ajuda"
    This reverts commit 583a639523f8b53631be512bdc9b1bea87d8a8f8.

--- js/circle.js
+++ js/circle.js
@@ -1,8 +1,3 @@
 $(".main-circle").click(function () {
   $(".other-circles").toggleClass("other-circles-active");
-  $(".help-button").toggleClass("help-button-show");
-});
-
-$(".help-button").click(function () {
-  $(".circle-text").toggleClass("circle-text-show");
 });
