583a639523f8b53631be512bdc9b1bea87d8a8f8
Juan Kineipe <juan.kineipe@gmail.com>
2020-05-04 11:53:30 -0300

    Adicionado botão de ajuda
    Botão para mostrar texto auxiliar se necessário

--- js/circle.js
+++ js/circle.js
@@ -1,3 +1,8 @@
 $(".main-circle").click(function () {
   $(".other-circles").toggleClass("other-circles-active");
+  $(".help-button").toggleClass("help-button-show");
+});
+
+$(".help-button").click(function () {
+  $(".circle-text").toggleClass("circle-text-show");
 });
